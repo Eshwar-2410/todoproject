379b97dbacc9f7a0aef67e86d5f5963b08424727
Eshwar-2410 <peelaeshwarmanideep.20.ece@anits.edu.in>
2024-12-06 12:43:33 +0530

    Fix: Add dj-database-url to requirements and update build script

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
